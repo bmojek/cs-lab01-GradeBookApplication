ce60f9ca40040bedef7ad54166cca94503724e76
Bartłomiej Mojek <Bartlomiej.Mojek@microsoft.wsei.edu.pl>
2022-03-06 12:51:22 +0100

    Update CreateCommand's Conditions

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,15 +34,16 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
-                return;
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
             }
             var name = parts[1];
             BaseGradeBook gradeBook = new BaseGradeBook(name);
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
+            //todo
+            
         }
 
         public static void LoadCommand(string command)
@@ -56,8 +57,7 @@ namespace GradeBook.UserInterfaces
             var name = parts[1];
             var gradeBook = BaseGradeBook.Load(name);
 
-            if (gradeBook == null)
-                return;
+  
 
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
